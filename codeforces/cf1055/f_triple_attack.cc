b485dfeb4b8871cd69b5497e605120b423c9de49
Linh Nguyen <linhvnguyen.mit@gmail.com>
2026-05-10 15:48:26 -0700

    CF1055

---
+++ codeforces/cf1055/f_triple_attack.cc
@@ -0,0 +1,124 @@
+#ifdef ONLINE_JUDGE
+#include <bits/stdc++.h>
+#endif
+
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+using ll = long long;
+
+static constexpr int MAXN = 250002;
+static constexpr int LOG = 20;
+
+int T;
+int n;
+long long z;
+int x[MAXN];
+int q;
+
+int nex[MAXN][LOG];
+int nex2[MAXN][LOG];
+ll step[MAXN][LOG];
+
+void solve_one() {
+  std::cin >> n >> z;
+  for (int i = 0; i < n; ++i) {
+    std::cin >> x[i];
+  }
+
+  nex[n][0] = n;
+  for (int i = 0; i < n; ++i) {
+    long long t = (long long)x[i] + z + 1;
+    nex[i][0] = (int)(std::lower_bound(x, x + n, t) - x);
+  }
+
+  for (int k = 0; k + 1 < LOG; ++k) {
+    for (int j = 0; j <= n; ++j) {
+      nex[j][k + 1] = nex[nex[j][k]][k];
+    }
+  }
+
+  for (int i = 0; i < n - 1; ++i) {
+    int cur1 = i;
+    int cur2 = i + 1;
+    int st = 0;
+    for (int j = LOG - 1; j >= 0; --j) {
+      if (nex[cur1][j] != nex[cur2][j] && nex[nex[cur1][j]][0] != nex[cur2][j]) {
+        cur1 = nex[cur1][j];
+        cur2 = nex[cur2][j];
+        st += 2 << j;
+      }
+    }
+    if (nex[cur1][0] == cur2) {
+      nex2[i][0] = nex[cur1][0];
+      step[i][0] = st + 1;
+    } else if (nex[cur1][0] == nex[cur2][0]) {
+      nex2[i][0] = nex[cur1][0];
+      step[i][0] = st + 2;
+    } else {
+      // Third collision type: parallel chains meet after one extra step on the left.
+      nex2[i][0] = nex[cur1][1];
+      step[i][0] = st + 3;
+    }
+  }
+  nex2[n - 1][0] = n;
+  nex2[n][0] = n;
+  step[n][0] = 0;
+
+  for (int k = 0; k + 1 < LOG; ++k) {
+    for (int j = 0; j <= n; ++j) {
+      nex2[j][k + 1] = nex2[nex2[j][k]][k];
+      step[j][k + 1] = step[j][k] + step[nex2[j][k]][k];
+    }
+  }
+
+  std::cin >> q;
+  while (q--) {
+    int L, R;
+    std::cin >> L >> R;
+    --L;
+    // Half-open interval [L, R): 1-based inclusive [L+1, R] -> L = l-1, R = r (exclusive end).
+    if (L + 1 == R) {
+      std::cout << 1 << '\n';
+      continue;
+    }
+    ll ret = 2;
+    int cur = L;
+    for (int j = LOG - 1; j >= 0; --j) {
+      if (nex2[cur][j] < R) {
+        ret += step[cur][j];
+        cur = nex2[cur][j];
+      }
+    }
+    if (cur + 1 == R) {
+      --ret;
+    } else {
+      int a = cur;
+      int b = cur + 1;
+      for (int j = LOG - 1; j >= 0; --j) {
+        if (nex[a][j] < R) {
+          a = nex[a][j];
+          ret += 1LL << j;
+        }
+        if (nex[b][j] < R) {
+          b = nex[b][j];
+          ret += 1LL << j;
+        }
+      }
+    }
+    std::cout << ret << '\n';
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  std::cin >> T;
+  while (T--) {
+    solve_one();
+  }
+  return 0;
+}
